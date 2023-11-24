a62c57bbae73e800333882a6db87b1f24023d502
Safi Ahmed <safi@brio.co.in>
2023-11-24 15:38:35 +0530

    added try catch and print statements to .py file for debugging. Moved function definitions to the top.

--- AzureCostVsSales.py
+++ AzureCostVsSales.py
@@ -5,17 +5,59 @@ import pandas as pd
 from azure.storage.blob import ContainerClient,BlobServiceClient
 from io import StringIO
 import numpy as np
-import calendar
 import json
 import datetime as dt
 import warnings
-import time
 
 warnings.filterwarnings('ignore')
 
 pd.set_option('display.max_columns', None)
 
 pd.set_option('display.max_rows', None)
+
+def getAccessToken(request_body):
+    req = rs.post(
+                  "https://login.windows.net/6e75cca6-47f0-47a3-a928-9d5315750bd9/oauth2/token"
+                  ,data = request_body)
+    access_token = json.loads(req.text)['access_token']
+    return access_token
+
+def getInvoices (base_url):
+    relative_url = '/v1/invoices'
+
+    response = json.loads(
+    rs.get(
+            f"{base_url}{relative_url}",
+           headers=headers)
+    .content)
+
+    main_df=pd.DataFrame() # Main DataFrame, currently empty
+
+    columns = list(response['items'][0].keys())
+
+    for row in response['items']:
+        row_df = pd.DataFrame.from_dict([row]) #Each row gets converted to a DataFrame
+        main_df = pd.concat([main_df,row_df],ignore_index=True) #Then gets appended to the Main DataFrame
+        
+    return main_df
+
+def getInvoiceLineItems(base_url, invoiceId):
+    relative_url = f'/v1/invoices/{invoiceId}/lineitems/OneTime/BillingLineItems?size=5000'
+
+    response = json.loads(
+        rs.get(
+                f"{base_url}{relative_url}",
+               headers=headers)
+        .content)
+
+    main_df=pd.DataFrame()
+
+    columns = list(response['items'][0].keys())
+
+    for row in response['items']:
+        sub_df = pd.DataFrame.from_dict([row])
+        main_df = pd.concat([main_df,sub_df],ignore_index=True)
+    return main_df
 
 secrets = json.load(open(file='./secrets.json'))
 
@@ -38,73 +80,44 @@ base_url = (
 'https://api.partnercenter.microsoft.com'
 )
 
-def getAccessToken(request_body):
-    req = rs.post(
-                  "https://login.windows.net/6e75cca6-47f0-47a3-a928-9d5315750bd9/oauth2/token"
-                  ,data = request_body)
-    access_token = json.loads(req.text)['access_token']
-    return access_token
+isAccessTokenValid = 0
+
+try:
+    access_token = getAccessToken(request_body = request_body)
+    print('Refresh token valid, Access token obtained.')
+    isAccessTokenValid = 1
+except:
+    print('Refresh token expired.')
 
 
-access_token = getAccessToken(request_body = request_body)
 
-headers = {'Authorization': 'Bearer ' + access_token}
 
-def getInvoices (base_url):
-    relative_url = '/v1/invoices'
 
-    response = json.loads(
-    rs.get(
-            f"{base_url}{relative_url}",
-           headers=headers)
-    .content)
 
-    main_df=pd.DataFrame() # Main DataFrame, currently empty
+if isAccessTokenValid == 1:
+    headers = {'Authorization': 'Bearer ' + access_token}
 
-    columns = list(response['items'][0].keys())
+    print('getting invoices...')
+    invoices_df = getInvoices(base_url = base_url)
 
-    for row in response['items']:
-        row_df = pd.DataFrame.from_dict([row]) #Each row gets converted to a DataFrame
-        main_df = pd.concat([main_df,row_df],ignore_index=True) #Then gets appended to the Main DataFrame
+    print('Filtering invoices to One-Time...')
+    invoices_df = invoices_df[invoices_df['invoiceType']=='OneTime']
+
+    invoiceIdList = list(invoices_df['id'])
+    invoiceDateList = list(invoices_df['invoiceDate'])
+
+    zipp = zip(invoiceIdList,invoiceDateList)
+
+
         
-    return main_df
 
-invoices_df = getInvoices(base_url = base_url)
-invoices_df = invoices_df[invoices_df['invoiceType']=='OneTime']
+    invoice_df = getInvoiceLineItems(base_url = base_url, invoiceId = 'G020077203')
 
-invoiceIdList = list(invoices_df['id'])
-invoiceDateList = list(invoices_df['invoiceDate'])
-
-zipp = zip(invoiceIdList,invoiceDateList)
-# latestInvoice,latestInvoiceDate = list(zipp)[-1] 
-
-def getInvoiceLineItems(base_url, invoiceId):
-    relative_url = f'/v1/invoices/{invoiceId}/lineitems/OneTime/BillingLineItems?size=5000'
-
-    response = json.loads(
-        rs.get(
-                f"{base_url}{relative_url}",
-               headers=headers)
-        .content)
-
-    main_df=pd.DataFrame()
-
-    columns = list(response['items'][0].keys())
-
-    for row in response['items']:
-        sub_df = pd.DataFrame.from_dict([row])
-        main_df = pd.concat([main_df,sub_df],ignore_index=True)
-    return main_df
-    
-
-invoice_df = getInvoiceLineItems(base_url = base_url, invoiceId = 'G020077203')
-invoice_df.head()
-
-print(invoice_df[
-    (invoice_df['customerName'].str.startswith('TATA'))
-          ][
-    ['productName','skuName','termAndBillingCycle','totalForCustomer','chargeStartDate','chargeEndDate']
-        ].sort_values('productName'))
+    print(invoice_df[
+        (invoice_df['customerName'].str.startswith('TATA'))
+            ][
+        ['productName','skuName','termAndBillingCycle','totalForCustomer','chargeStartDate','chargeEndDate']
+            ].sort_values('productName'))
 
 
 
